0a79bebb9d5027cb7877d8a78ba24cd8513022c5
eriklins <erik@lins.de>
2023-02-18 17:46:39 +0100

    Added vsp terminal .pas and .lfm

---
+++ src/uartterminal.pas
@@ -0,0 +1,30 @@
+unit UartTerminal;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, SynEdit,
+  SynPluginSyncroEdit;
+
+type
+
+  { TTerminalForm }
+
+  TTerminalForm = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  TerminalForm: TTerminalForm;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
